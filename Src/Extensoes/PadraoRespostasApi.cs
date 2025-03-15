378a9ffafd2313cb2c2720ca12d5975850c9c784
Ruan <ruanalmeidawegner@gmail.com>
2025-03-15 18:53:37 -0300

    feat Extensoes: Implementada a classe que padroniza as respotas da API.

--- Src/Extensoes/PadraoRespostasApi.cs
+++ Src/Extensoes/PadraoRespostasApi.cs
@@ -1,7 +1,22 @@
-﻿namespace Extensoes
-{
-    public class PadraoRespostasApi
-    {
+﻿using System.Net;
 
+namespace Extensoes
+{
+    public class PadraoRespostasApi<T>
+    {
+        public T? Dados { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        public HttpStatusCode HttpStatusCode { get; set; } = HttpStatusCode.OK;
+
+        public static PadraoRespostasApi<T> CriarResposta<T>(T? dados, string mensagem = "Ok", HttpStatusCode httpStatusCode = HttpStatusCode.OK)
+        {
+            return new PadraoRespostasApi<T>
+            {
+                Dados = dados,
+                Mensagem = mensagem,
+                HttpStatusCode = httpStatusCode
+
+            };
+        }
     }
 }
