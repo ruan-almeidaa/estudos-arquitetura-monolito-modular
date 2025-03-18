2cc4f13f19e0b48e7c59b49f4f1c9aa75af6f9ec
Ruan <ruanalmeidawegner@gmail.com>
2025-03-18 20:44:12 -0300

    feat Extensoes: Disponibilizada classe responsável por gerar hash.

---
+++ Src/Extensoes/Criptografia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensoes
+{
+    public static class Criptografia
+    {
+        public static string GerarHash(this string valorParaConverter)
+        {
+            var hash = SHA1.Create();
+            var encode = new ASCIIEncoding();
+            var array = encode.GetBytes(valorParaConverter);
+            array = hash.ComputeHash(array);
+
+            var strHexa = new StringBuilder();
+
+            foreach (var item in array)
+            {
+                strHexa.Append(item.ToString("x2"));
+            }
+
+            return strHexa.ToString();
+        }
+    }
+}
