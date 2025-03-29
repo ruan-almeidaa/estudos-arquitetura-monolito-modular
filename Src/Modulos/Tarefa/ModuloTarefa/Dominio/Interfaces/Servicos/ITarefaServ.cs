b06c3ad499fdcc8cfd0f610c44e74614cdb391a7
Ruan <ruanalmeidawegner@gmail.com>
2025-03-29 10:52:02 -0300

    feat Modulo Tarefa: Implementado serviço.

---
+++ Src/Modulos/Tarefa/ModuloTarefa/Dominio/Interfaces/Servicos/ITarefaServ.cs
@@ -0,0 +1,19 @@
+﻿using ModuloTarefa.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloTarefa.Dominio.Interfaces.Servicos
+{
+    public interface ITarefaServ
+    {
+        Task<Tarefa> CriarTarefa(Tarefa tarefa);
+        Task<Tarefa> BuscarTarefaPorId(int id);
+        Task<Tarefa> EditarTarefa(Tarefa tarefa);
+        Task<bool> ExcluirTarefa(int idTarefa);
+        Task<List<Tarefa>> BuscarTodasTarefas(int numeroPagina, int totalItens);
+        Task<int> ContarTarefas();
+    }
+}
---
+++ Src/Modulos/Tarefa/ModuloTarefa/Dominio/Servicos/TarefaServ.cs
@@ -0,0 +1,49 @@
+﻿using ModuloTarefa.Dominio.Interfaces.Repositorios;
+using ModuloTarefa.Dominio.Interfaces.Servicos;
+using ModuloTarefa.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloTarefa.Dominio.Servicos
+{
+    public class TarefaServ : ITarefaServ
+    {
+        private readonly ITarefaRepo _tarefaRepo;
+        public TarefaServ(ITarefaRepo tarefaRepo)
+        {
+            _tarefaRepo = tarefaRepo;
+        }
+        public async Task<Tarefa> BuscarTarefaPorId(int id)
+        {
+            return await _tarefaRepo.BuscarTarefaPorId(id);
+        }
+
+        public async Task<List<Tarefa>> BuscarTodasTarefas(int numeroPagina, int totalItens)
+        {
+            return await _tarefaRepo.BuscarTodasTarefas(numeroPagina, totalItens);
+        }
+
+        public async Task<int> ContarTarefas()
+        {
+            return await _tarefaRepo.ContarTarefas();
+        }
+
+        public async Task<Tarefa> CriarTarefa(Tarefa tarefa)
+        {
+            return await _tarefaRepo.CriarTarefa(tarefa);
+        }
+
+        public async Task<Tarefa> EditarTarefa(Tarefa tarefa)
+        {
+            return await _tarefaRepo.EditarTarefa(tarefa);
+        }
+
+        public async Task<bool> ExcluirTarefa(int idTarefa)
+        {
+            return await _tarefaRepo.ExcluirTarefa(await BuscarTarefaPorId(idTarefa));
+        }
+    }
+}
