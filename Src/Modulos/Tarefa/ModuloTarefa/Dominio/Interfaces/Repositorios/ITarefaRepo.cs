153efbdaebf528c195b46c2a0b97d15e5842f01c
Ruan <ruanalmeidawegner@gmail.com>
2025-03-28 23:22:23 -0300

    feat Modulo Tarefa: Criada interface e classe para o repositório

---
+++ Src/Modulos/Tarefa/ModuloTarefa/Dominio/Interfaces/Repositorios/ITarefaRepo.cs
@@ -0,0 +1,19 @@
+﻿using ModuloTarefa.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloTarefa.Dominio.Interfaces.Repositorios
+{
+    public interface ITarefaRepo
+    {
+        Task<Tarefa> CriarTarefa(Tarefa tarefa);
+        Task<Tarefa> BuscarTarefaPorId(int id);
+        Task<Tarefa> EditarTarefa(Tarefa tarefa);
+        Task<bool> ExcluirTarefa(Tarefa tarefa);
+        Task<List<Tarefa>> BuscarTodasTarefas(int numeroPagina, int totalItens);
+        Task<int> ContarTarefas();
+    }
+}
---
+++ Src/Modulos/Tarefa/ModuloTarefa/Infra/Repositorios/TarefaRepo.cs
@@ -0,0 +1,43 @@
+﻿using ModuloTarefa.Dominio.Interfaces.Repositorios;
+using ModuloTarefa.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloTarefa.Infra.Repositorios
+{
+    public class TarefaRepo : ITarefaRepo
+    {
+        public Task<Tarefa> BuscarTarefaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tarefa>> BuscarTodasTarefas(int numeroPagina, int totalItens)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> ContarTarefas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tarefa> CriarTarefa(Tarefa tarefa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tarefa> EditarTarefa(Tarefa tarefa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExcluirTarefa(Tarefa tarefa)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
