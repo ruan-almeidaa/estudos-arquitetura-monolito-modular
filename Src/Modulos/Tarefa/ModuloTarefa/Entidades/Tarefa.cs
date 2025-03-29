f19958c33a75dc705d08f2e608fda6139f34518d
Ruan <ruanalmeidawegner@gmail.com>
2025-03-28 23:13:51 -0300

    feat Modulo Tarefa: Criada entidade de Tarefa e o enum de status.

---
+++ Src/Modulos/Tarefa/ModuloTarefa/Entidades/Tarefa.cs
@@ -0,0 +1,20 @@
+﻿using ModuloTarefa.Enumeradores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloTarefa.Entidades
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public required string Titulo { get; set; }
+        public required string Descricao { get; set; }
+        public required DateTime DataCriacao { get; set; }
+        public DateTime? DataConclusao { get; set; }
+        public required int UsuarioId { get; set; }
+        public required StatusTarefa Status { get; set; } = StatusTarefa.Pendente;
+    }
+}
---
+++ Src/Modulos/Tarefa/ModuloTarefa/Enumeradores/StatusTarefa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloTarefa.Enumeradores
+{
+    public enum StatusTarefa
+    {
+        [Description("Pendente")]
+        Pendente = 0,
+        [Description("Em andamento")]
+        EmAndamento = 1,
+        [Description("Concluída")]
+        Concluida = 2
+    }
+}
