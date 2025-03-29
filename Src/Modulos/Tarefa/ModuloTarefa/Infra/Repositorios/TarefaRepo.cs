08d026986773a506aa2a05813013792445883fc4
Ruan <ruanalmeidawegner@gmail.com>
2025-03-29 10:29:42 -0300

    feat Modulo Tarefa: Implementado repositorio.

--- Src/Modulos/Tarefa/ModuloTarefa/Infra/Repositorios/TarefaRepo.cs
+++ Src/Modulos/Tarefa/ModuloTarefa/Infra/Repositorios/TarefaRepo.cs
@@ -1,5 +1,7 @@
-﻿using ModuloTarefa.Dominio.Interfaces.Repositorios;
+﻿using Microsoft.EntityFrameworkCore;
+using ModuloTarefa.Dominio.Interfaces.Repositorios;
 using ModuloTarefa.Entidades;
+using ModuloTarefa.Infra.Banco;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,34 +12,53 @@ namespace ModuloTarefa.Infra.Repositorios
 {
     public class TarefaRepo : ITarefaRepo
     {
-        public Task<Tarefa> BuscarTarefaPorId(int id)
+        private readonly ConfiguracaoContextoBancoModuloTarefa _contexto;
+        public TarefaRepo(ConfiguracaoContextoBancoModuloTarefa contexto)
         {
-            throw new NotImplementedException();
+            _contexto = contexto;
+        }
+        public async Task<Tarefa> BuscarTarefaPorId(int id)
+        {
+            return await _contexto.Tarefas
+                .AsNoTracking()
+                .FirstOrDefaultAsync(t => t.Id == id);
         }
 
-        public Task<List<Tarefa>> BuscarTodasTarefas(int numeroPagina, int totalItens)
+        public async Task<List<Tarefa>> BuscarTodasTarefas(int numeroPagina, int totalItens)
         {
-            throw new NotImplementedException();
+            return await _contexto.Tarefas
+                .AsNoTracking()
+                .OrderBy(t => t.Id)
+                .Skip((numeroPagina - 1) * totalItens) // Pula os registros das páginas anteriores
+                .Take(totalItens) // Pega apenas os registros da página atual
+                .ToListAsync();
         }
 
-        public Task<int> ContarTarefas()
+        public async Task<int> ContarTarefas()
         {
-            throw new NotImplementedException();
+            return await _contexto.Tarefas.CountAsync();
         }
 
-        public Task<Tarefa> CriarTarefa(Tarefa tarefa)
+        public async Task<Tarefa> CriarTarefa(Tarefa tarefa)
         {
-            throw new NotImplementedException();
+            await _contexto.Tarefas.AddAsync(tarefa);
+            await _contexto.SaveChangesAsync();
+            return await BuscarTarefaPorId(tarefa.Id);
         }
 
-        public Task<Tarefa> EditarTarefa(Tarefa tarefa)
+        public async Task<Tarefa> EditarTarefa(Tarefa tarefa)
         {
-            throw new NotImplementedException();
+            _contexto.ChangeTracker.Clear();
+            _contexto.Tarefas.Update(tarefa);
+            await _contexto.SaveChangesAsync();
+            return await BuscarTarefaPorId(tarefa.Id);
         }
 
-        public Task<bool> ExcluirTarefa(Tarefa tarefa)
+        public async Task<bool> ExcluirTarefa(Tarefa tarefa)
         {
-            throw new NotImplementedException();
+            _contexto.Tarefas.Remove(tarefa);
+            int qtdRegistrosExcluidos = await _contexto.SaveChangesAsync();
+            return qtdRegistrosExcluidos > 0;
         }
     }
 }
