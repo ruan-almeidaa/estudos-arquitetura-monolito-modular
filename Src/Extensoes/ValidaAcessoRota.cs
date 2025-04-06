6bc233292bf7ff58de8de1c1ef34cd47a620f0e7
Ruan <ruanalmeidawegner@gmail.com>
2025-04-06 15:50:42 -0300

    refactor Extensoes: Na classe "ValidaAcessoRota", ajustado nome do parâmetro que recebe o id do usuário que está fazendo a requisição.

--- Src/Extensoes/ValidaAcessoRota.cs
+++ Src/Extensoes/ValidaAcessoRota.cs
@@ -8,9 +8,9 @@ namespace Extensoes
 {
     public static class ValidaAcessoRota
     {
-        public static void ValidarAcessoRota(int idTokenUsuario, int idUsuario, string roleTokenUsuario, bool adminDeveAcessar)
+        public static void ValidarAcessoRota(int idTokenUsuario, int idUsuarioReq, string roleTokenUsuario, bool adminDeveAcessar)
         {
-            if (idTokenUsuario != idUsuario)
+            if (idTokenUsuario != idUsuarioReq)
             {
                 if((roleTokenUsuario != "Administrador") || (roleTokenUsuario == "Administrador" && !adminDeveAcessar))
                 {
